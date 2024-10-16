06518168919e7fbe248fe352a2bfe441a49d0c2d
Rhyan <lucas.rhyan@aluno.ifsertao-pe.edu.br>
2024-10-16 13:00:06 -0300

    3rd party api connection update

--- assets/api_connection.py
+++ assets/api_connection.py
@@ -1,26 +1,34 @@
 import requests
+from urllib.parse import urljoin, urlencode
+
 
 class ApiConnection:
     def __init__(self):
         self.base_url = 'https://brapi.dev/api/'
         self.token = "token="
 
-    def construct_url(self,endpoint):
-        return self.base_url + endpoint + self.token
+    def construct_url(self,endpoint, params = None):
+        url = urljoin(self.base_url, endpoint)
+        if params is None:
+            params = {}
+        params['token'] = self.token
+        query_string = urlencode(params)
+        return f"{url}?{query_string}"
 
     def get_asset_data_by_ticker(self,ticker):
-        endpoint = "quote/?"
-        response = requests.get(self.construct_url(endpoint))
+        endpoint = "quote/"
+        params = {'symbol': ticker}
+        response = requests.get(self.construct_url(endpoint,params))
         if response.status_code == 200:
             return response.json()
         return None
 
-
-    def search_by_ticker(self,ticker)->list[str]:
-        endpoint = "list/?search=" + ticker + "&"
-
-        response = requests.get(self.construct_url(endpoint))
+    def search_by_ticker(self,ticker)->list:
+        endpoint = "list/"
+        params = {'search': ticker}
+        url =self.construct_url(endpoint,params)
+        response = requests.get(self.construct_url(endpoint,params))
         if response.status_code == 200:
             return response.json()
-        return [""]
+        return []
 
