6f87ec1531812cb92b2537db83cf04a3ccf19489
Rhyan <lucas.rhyan@aluno.ifsertao-pe.edu.br>
2024-11-16 12:16:26 -0300

    portfolio api adjust

--- portfolio/serializers.py
+++ portfolio/serializers.py
@@ -8,8 +8,17 @@ class PortfolioAssetSerializer(serializers.ModelSerializer):
         fields = ['asset','portfolio','quantity','average_price']
 
 class PortfolioSerializer(serializers.ModelSerializer):
-    user = serializers.CharField(source='user.username')
+    user = serializers.CharField(source='user.username',read_only=True)
+    total = serializers.DecimalField(max_digits=10, decimal_places=2)
+    appreciation = serializers.DecimalField(max_digits=10, decimal_places=2)
 
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.total = validated_data.get('total', instance.total)
+        instance.appreciation = validated_data.get('appreciation', instance.appreciation)
+        instance.followers = validated_data.get('followers', instance.followers)
+        instance.save()
+        return instance
     class Meta:
         model = Portfolio
-        fields = ['id','user','title', 'total', 'appreciation']
+        fields = ['id','user','title', 'total', 'appreciation', 'followers']
