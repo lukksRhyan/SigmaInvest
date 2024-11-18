f229f418461b937abb275947ee29a1c3df576ab9
Rhyan <lucas.rhyan@aluno.ifsertao-pe.edu.br>
2024-11-18 17:28:09 -0300

    portfolio serializer adjust

--- portfolio/serializers.py
+++ portfolio/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from .models import Portfolio,PortfolioAsset
+from .models import Portfolio, PortfolioAsset, User
+
 
 class PortfolioAssetSerializer(serializers.ModelSerializer):
 
@@ -8,9 +9,10 @@ class PortfolioAssetSerializer(serializers.ModelSerializer):
         fields = ['asset','portfolio','quantity','average_price']
 
 class PortfolioSerializer(serializers.ModelSerializer):
-    user = serializers.CharField(source='user.username')
-    total = serializers.DecimalField(max_digits=10, decimal_places=2, read_only=True)
-    appreciation = serializers.DecimalField(max_digits=10, decimal_places=2, read_only=True)
+    username = serializers.ReadOnlyField(source='user.username')
+    total = serializers.DecimalField(max_digits=15, decimal_places=7,read_only=True)
+    appreciation = serializers.DecimalField(max_digits=15, decimal_places=7, read_only=True)
+    followers = serializers.DecimalField(max_digits=10, decimal_places=0,read_only=True     )
 
     def update(self, instance, validated_data):
         instance.title = validated_data.get('title', instance.title)
@@ -19,6 +21,14 @@ class PortfolioSerializer(serializers.ModelSerializer):
         instance.followers = validated_data.get('followers', instance.followers)
         instance.save()
         return instance
+
+    def create(self, validated_data):
+        username = validated_data.pop('user', None)
+        user = User.objects.get(username=username)
+        portfolio = Portfolio.objects.create(user = user,**validated_data)
+
+        return portfolio
+
     class Meta:
         model = Portfolio
-        fields = ['id','user','title', 'total', 'appreciation', 'followers']
+        fields = ['id','user','username','title', 'total', 'appreciation', 'followers']
