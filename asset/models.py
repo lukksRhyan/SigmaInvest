790bb57f485afacf109911f4deb1baa0e465698b
Rhyan <lucas.rhyan@aluno.ifsertao-pe.edu.br>
2025-01-20 17:35:39 -0300

    asset transaction logic

--- asset/models.py
+++ asset/models.py
@@ -7,7 +7,7 @@ import asset.api_connection as api
 class Asset(models.Model):
     ticker = models.CharField(max_length=10, unique=True, null=False)# Equivalente a coin para cripto e currency para moedas
     fullname = models.CharField(max_length=10, unique=False, null=True)
-    sector = models.CharField(max_length=30, unique=False,default='None')
+    sector = models.CharField(max_length=30,default='None')
 
     def __str__(self):
         return self.ticker
