34d5975c9bc85363cc54396fe805432ccedb7d28
Rhyan <lucas.rhyan@aluno.ifsertao-pe.edu.br>
2025-02-18 17:21:16 -0300

    WSGI changes

--- SigmaInvest/wsgi.py
+++ SigmaInvest/wsgi.py
@@ -1,7 +1,8 @@
 import os
-
 from django.core.wsgi import get_wsgi_application
 
+# Garante que o Django escute na porta correta do Railway
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SigmaInvest.settings')
+os.environ.setdefault('PORT', '8000')  # Define um padrão para rodar localmente
 
 application = get_wsgi_application()
